52e363f4d21dce0b957ccfe8f50a5e58074f1d08
sheeluvikas <vikaskatiyar@live.com>
2017-08-05 03:31:51 +0530

    db changes

--- src/main/java/demo/MainClass.java
+++ src/main/java/demo/MainClass.java
@@ -1,8 +1,14 @@
 package demo;
 
 import java.sql.*;
+import java.util.List;
 
+import beans.Event;
+import beans.ForestAuthority;
+import beans.Location;
+import beans.Login;
 import database.DBConnection;
+import database.DBUtils;
 
 class MainClass {
 	
@@ -15,20 +21,48 @@ class MainClass {
 			
 			con = dbConnection.getConnection();
 
-			// step3 create the statement object
-			Statement stmt = con.createStatement();
-
-			// step4 execute query
-			System.out.println("Location");
-			ResultSet rs = stmt.executeQuery("select * from location");
-			while (rs.next())
-				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			String query = "select location_id, "
+							+ "longitude, latitude, city from LOCATION "
+							+ " where location_id = '100002' ";
+			String eventQuery = "SELECT EVENT_ID, LATITUDE, LONGITUDE, THREAT_LEVEL,"
+					+ " STATUS, AUTHORITY_ID, LOGGED_TIME, EVENT_TYPE FROM EVENT ";
+			String forest_authority_query = "SELECT ID, TYPE_C, LOCATION_ID FROM FOREST_AUTHORITY ";
+			String login_query = "SELECT USER_NAME, ROLE, PASSWORD FROM LOGIN";
+			//*******************
+			System.out.println("*******Location******");
+			List<Location> location = (List<Location>)DBUtils.getDataFromTable(con, new Location(),
+					query);
+			
+			for(Location loc : location){
+				System.out.println(loc.getCity());
+			}
+			//**********************
+			System.out.println("********Event********");
+			List<Event> eventList = (List<Event>)DBUtils.getDataFromTable(con, new Event(),
+					eventQuery);
+			for(Event event : eventList){
+				System.out.println(event.getId()+", "+event.getEventType());
+			}
+			//***************
+			System.out.println("********Forest Authority*******");
+			List<ForestAuthority> forestList = (List<ForestAuthority>)DBUtils.getDataFromTable(con, new ForestAuthority(),
+					forest_authority_query);
+			for(ForestAuthority authority : forestList){
+				System.out.println(authority.getId()+", "+authority.getType()+", "+authority.getLocation().getCity());
+			}
+			//****************
+			System.out.println("******Login*******");
+			List<Login> loginList = (List<Login>)DBUtils.getDataFromTable(con, new Login(),
+					login_query);
+			for(Login login : loginList){
+				System.out.println(login.getUserName()+", "+login.getRole());
+			}
 			
 			
-			// step5 close the connection object
 			con.close();
 
 		} catch (Exception e) {
+			System.out.println("Exception 0");
 			System.out.println(e);
 		}
 
